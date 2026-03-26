d418f9348f5d12b0752a2608fcecb60b2536efc0
tahakassar97 <tahakassar54@gmail.com>
2026-03-26 14:07:27 +0400

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const layerDir = dirname(fileURLToPath(import.meta.url))
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    join(__dirname, 'components/**/*.{js,vue,ts}'),
-    join(__dirname, 'layouts/**/*.vue'),
-    join(__dirname, 'pages/**/*.vue'),
-    join(__dirname, 'app.{js,vue,ts}'),
-    join(__dirname, 'App.{js,vue,ts}'),
+    join(layerDir, 'components/**/*.{js,vue,ts}'),
+    join(layerDir, 'layouts/**/*.vue'),
+    join(layerDir, 'pages/**/*.vue'),
+    join(layerDir, 'app.{js,vue,ts}'),
+    join(layerDir, 'App.{js,vue,ts}'),
   ],
   theme: {
     extend: {
